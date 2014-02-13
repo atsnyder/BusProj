a4e6de157fabe0feb29f2b1fcb1d2c9c5bdab41e
saasbook <saasbook@saasbook.(none)>
2014-02-13 12:48:06 -0800

    Initial Commit by ND

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BusRide::Application.config.secret_token = 'd891fd6f0eaa2eee6af9afad17dec27e734a77b66e32bc480906847696f04f54465508dc727f8dd56014731c8d60710be65e9bbcca80a0ca5ff7edd167f991b5'
