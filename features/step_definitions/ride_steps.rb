32da2662953b6c7c9166b9119499fb333e6cbea7
weiqiyang <weiqi-yang@uiowa.edu>
2014-02-21 11:51:32 -0600

    Modified cucumber test code for searching

--- features/step_definitions/ride_steps.rb
+++ features/step_definitions/ride_steps.rb
@@ -3,12 +3,12 @@ Given /^I am on the BusRide home page$/ do
  end
 
 Given(/^I have searched for a bus with source "(.*?)" and initial departure date "(.*?)" and destination "(.*?)" and return departure date "(.*?)" and number of passengers "(.*?)" and I click the search button$/) do |from, from_date, to, to_date, passengers|
-  fill_in 'To', :with => to
-  fill_in 'From', :with => from
-  fill_in 'To_Date', :with => to_date
-  fill_in 'From_Date', :with => from_date
-  fill_in '# of passengers', :with => passengers
-  click_button "Search"
+  fill_in 'to_search_box', :with => to
+  fill_in 'from_search_box', :with => from
+  fill_in 'to_datepicker', :with => to_date
+  fill_in 'from_datepicker', :with => from_date
+  fill_in 'passengers_box', :with => passengers
+  click_button "search"
 end
 
 Then(/^I should be on the Search Results page$/) do
