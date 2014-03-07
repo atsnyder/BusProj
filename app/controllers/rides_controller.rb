100428f8fda093adfcfe0ef76ed08c5f68427715
weiqiyang <weiqi-yang@uiowa.edu>
2014-03-07 12:49:03 -0600

    round trip search

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -11,9 +11,18 @@ def show
 
 end
 
-def dosearch    
-     @rides = Ride.fetch_results params[:search]
-     if(@rides.size == 0)
+def dosearch
+    @round_trip = false
+    @rides = Ride.fetch_results params[:search]
+    if(params[:search]["radio"] == "RoundTrip")
+      @round_trip = true
+      @rides2 = Ride.fetch_back_results params[:search]
+      if(@rides2.size == 0)
+        flash[:notice] = "No routes found match the search terms."
+        redirect_to rides_path
+      end
+    end
+    if(@rides.size == 0)
       flash[:notice] = "No routes found match the search terms."
       redirect_to rides_path
     end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -7,6 +7,10 @@ def self.fetch_results params
 	return rides
 end
 
+def self.fetch_back_results params
+	rides = Ride.find_by_sql("SELECT * from 2014March8 WHERE DEPART_CITY like '%#{params[:to_city]}%' AND ARRIVE_CITY like '%#{params[:from_city]}%'")
+	return rides
+end
 
 end
 
