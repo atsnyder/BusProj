7ec026908e02dd38a5e4c0e651f0f01b518db3d6
weiqiyang <weiqi-yang@uiowa.edu>
2014-04-08 16:49:32 -0500

    round/go trip button

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -12,17 +12,17 @@ end
 
 def dosearch
 
+    params[:search] = params[:search] || {"radio" => cookies[:radio], "from_city" => cookies[:from], "to_city" => cookies[:to]}
     params[:date] = params[:date] || {"from_Date" => cookies[:from_date], "to_Date" => cookies[:to_date]}
 
-    cookies[:radio] = "OneWay" || params[:search]["radio"]
+    cookies[:radio] = params[:search]["radio"] || cookies[:radio]
 
-
-    cookies[:from] = cookies[:from] || params[:search]["from_city"]
-    cookies[:to] = cookies[:to] || params[:search]["to_city"]
+    cookies[:from] = params[:search]["from_city"] || cookies[:from]
+    cookies[:to] = params[:search]["to_city"] || cookies[:to]
 
     cookies[:from_date] = params[:date]["from_Date"] || cookies[:from_date]  
     cookies[:to_date] = params[:date]["to_Date"] ||  cookies[:to_date] 
-       
+
     params[:search] = params[:search] || {"radio" => cookies[:radio], "from_city" => cookies[:from], "to_city" => cookies[:to]}
 
     params[:date]["from_Date"] = params[:date]["from_Date"] || cookies[:from_date]
@@ -38,21 +38,19 @@ def dosearch
     @rides = Kaminari.paginate_array(@rides).page(params[:page]).per(@perpage)
     if(params[:search]["radio"] == "RoundTrip")
       @round_trip = true
-      @rides2 = Ride.fetch_back_results(params[:search],params[:date])
-      #@rides2 = Kaminari.paginate_array(@rides2).page(params[:page]).per(@perpage)
+      if(params[:search]["button_tag"].nil?)
+        @button_tag = true
+      else
+        @button_tag = (!eval(params[:search]["button_tag"])) || false
+      end
+      @back_from = params[:search]["to_city"]
+      @back_to = params[:search]["from_city"]
+      @from_date = params[:date]["to_Date"]
+      @to_date = params[:date]["from_Date"]
     end
     if(@rides.size == 0)
       flash[:notice] = "No routes found match the search terms."
       redirect_to rides_path
-    elsif(@round_trip == true)
-      if(@rides2.size == 0)
-        flash[:notice] = "No routes found match the search terms."
-        redirect_to rides_path
-      else
-        render :layout=>"frameset"
-      end
-    else
-      render :layout=>"frameset"
     end
 end
 
