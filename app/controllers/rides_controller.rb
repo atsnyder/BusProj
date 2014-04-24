8275e951fe44f3c6a8954070cfb4a086024151a6
atsnyder <adam-snyder@uiowa.edu>
2014-04-23 18:36:48 -0700

    api

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,13 +1,47 @@
 class RidesController < ApplicationController
 
 def index
-
+  @rides = Ride.all
+  respond_to do |format|
+    format.html
+    format.xml  {render}
+    format.json {render}
+  end
 end
 
 def new
 end
 
 def show
+
+    @rides = Ride.fetch_android_results(params[:search],params[:date])
+    
+    @rides = Kaminari.paginate_array(@rides).page(params[:page]).per(@perpage)
+    if(params[:search]["radio"] == "OneWay" || params[:date]["to_Date"] == "")
+      @round_trip = false
+    else
+      if(cookies[:submit_btn]=="go")
+        @button_tag = true
+      else
+        @button_tag = false
+      end
+      @back_from = params[:search]["to_city"]
+      @back_to = params[:search]["from_city"]
+      @from_date = params[:date]["to_Date"]
+      @to_date = params[:date]["from_Date"]
+    end
+#    if(@rides.size == 0)
+#      flash[:notice] = "No routes found matching the search terms."
+#      redirect_to rides_path
+#    end
+    
+    @search = @rides
+    respond_to do |format|
+      format.html
+      format.xml  {render}
+      format.json {render}
+    end
+
 end
 
 def dosearch
@@ -57,6 +91,13 @@ def dosearch
     if(@rides.size == 0)
       flash[:notice] = "No routes found matching the search terms."
       redirect_to rides_path
+    end
+    
+    @search = @rides
+    respond_to do |format|
+      format.html
+      format.xml  {render}
+      format.json {render}
     end
 end
 
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -30,12 +30,58 @@ def self.fetch_back_results (params,paramsdate)
 
 end
 
+def self.fetch_android_results (params,paramsdate)
+	params[:from_city].slice! ", United States"
+	params[:to_city].slice! ", United States"
+  puts params
+  puts paramsdate
+	table = tabledroidname(paramsdate[:from_Date])         
+	rides = Ride.find_by_sql("SELECT * from #{table} WHERE DEPART_CITY like '%#{params[:from_city]}%' AND ARRIVE_CITY like '%#{params[:to_city]}%'")
+	return rides.to_a
+
+end
+
+def self.tabledroidname table
+	  tempdate = table.split('/')
+	  case tempdate[0]
+	  when '1'
+	    tempdate[0] = 'January'
+	  when '2'
+	    tempdate[0] = 'February'
+	  when '3'
+	    tempdate[0] = 'March'
+	  when '4'
+	    tempdate[0] = 'April'
+	  when '5'
+	    tempdate[0] = 'May'
+	  when '6'
+	    tempdate[0] = 'June'
+	  when '7'
+	    tempdate[0] = 'July'
+	  when '8'
+	    tempdate[0] = 'August'
+	  when '9'
+	    tempdate[0] = 'September'
+	  when '10'
+	    tempdate[0] = 'October'
+	  when '11'
+	    tempdate[0] = 'November'
+	  when '12'
+	    tempdate[0] = 'December'
+	  else
+	    tempdate[0] = "goofed"
+	  end
+	  tablename = (tempdate[2]+tempdate[0]+tempdate[1])
+	  return tablename
+end
+
 def self.tablename table
 	  year = table[(table.length)-4..table.length]	
 	  day = ((table[(table.length-7)..(table.length-6)]).to_i).to_s
 	  monthkey = table[4..(table.length-9)]	 
 	  monthvalue = @month[monthkey]
 	  tablename = (year+monthvalue+day)
+	  puts(tablename)
 	  return tablename
 end
 
